4807c522f925da891ee7208a1307b8b0b4d1bcdd
drumer2142 <fotis360@windowslive.com>
2022-12-04 03:15:32 +0200

    remove repo and fix inf errors

--- src/api/database/dbconnect.go
+++ src/api/database/dbconnect.go
@@ -29,3 +29,27 @@ func Connect() (*RepositoryGorm, error) {
 	}, nil
 
 }
+
+func (r *RepositoryGorm) StoreWebSite(*models.Website) error {
+
+}
+
+func (r *RepositoryGorm) RetriveAllWebSites() ([]*models.Website, error) {
+	// var err error
+	// allsites := []models.Website{}
+	// err = r.db.Find(&allsites).Error
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return allsites, nil
+}
+
+func (r *RepositoryGorm) RetriveByDomainName(domain string) (*models.Website, error) {
+	// var err error
+	// var sitebydomain []models.Website
+	// err = r.db.Debug().Where("domain = ?", domain).Find(&sitebydomain).Error
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return sitebydomain, nil
+}
